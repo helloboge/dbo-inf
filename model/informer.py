14d04594a94ad52295d22bf9d9b22544a65ab71f
helloboge <65157937+helloboge@users.noreply.github.com>
2023-12-04 21:18:11 +0800

    Update informer.py

--- model/informer.py
+++ model/informer.py
@@ -21,7 +21,6 @@ class Informer(nn.Module):
         d_mark=4,
         dropout=0.1,
         c=5,
-        features='MS',
     ):
         super(Informer, self).__init__()
 
@@ -37,7 +36,6 @@ class Informer(nn.Module):
             d_mark=d_mark,
             dropout=dropout,
             c=c,
-            features='MS',
         )
         self.decoder = Decoder(
             d_k=d_k,
@@ -50,7 +48,6 @@ class Informer(nn.Module):
             d_mark=d_mark,
             dropout=dropout,
             c=c,
-            features='MS',
         )
 
         self.projection = nn.Linear(d_model, d_feature, bias=True)
